9b2701d46750aebf5f98aed11cecfba31df32dbb
dhirajkpandey <dhirajk04@gmail.com>
2018-07-22 14:49:23 +0200

    Check in all files

---
+++ CrefoPayTest/src/test/CardValidatorTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import main.CardValidator;
+
+public class CardValidatorTest {
+ 
+	@Test
+	public void verifyCardIsValidAndIsOfVisaOrMasterType() {
+		CardValidator.checkValidCardNumberAndType();
+		Reporter.log("Card validation test passed successfully", true);
+		
+	}
+}
